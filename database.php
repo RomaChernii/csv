ecd4f099b0b62e85afad75036fb4267d018b64c1
Roma Chernii <roma1996ua@gmail.com>
2017-08-02 19:04:34 +0300

    Init

--- database.php
+++ database.php
@@ -2,6 +2,6 @@
   $link=mysqli_connect('localhost','root','','warehouse');
   mysqli_query($link, 'set names utf8 collate utf8_general_ci');
   if(mysqli_connect_errno()){
-	echo 'Ошыбка в подключении к бази данных('.mysqli_connect_errno().'): '.mysqli_connect_error();
+	echo 'Error connecting to database('.mysqli_connect_errno().'): '.mysqli_connect_error();
 	exit();
   }
